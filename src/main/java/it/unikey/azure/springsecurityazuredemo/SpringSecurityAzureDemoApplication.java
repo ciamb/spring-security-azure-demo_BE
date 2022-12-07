a3c21867d4fdb6a28f5df6d3d587c9d5c21fdc69
Andrea Ciambella <andrea.ciambella@unikey.it>
2022-12-07 16:28:19 +0100

    demo funzionante con file.yml

--- src/main/java/it/unikey/azure/springsecurityazuredemo/SpringSecurityAzureDemoApplication.java
+++ src/main/java/it/unikey/azure/springsecurityazuredemo/SpringSecurityAzureDemoApplication.java
@@ -2,14 +2,10 @@ package it.unikey.azure.springsecurityazuredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class SpringSecurityAzureDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/it/unikey/azure/springsecurityazuredemo/controller/HelloController.java
+++ src/main/java/it/unikey/azure/springsecurityazuredemo/controller/HelloController.java
@@ -1,7 +1,9 @@
 package it.unikey.azure.springsecurityazuredemo.controller;
 
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -10,12 +12,14 @@ import java.security.Principal;
 public class HelloController {
 
     @GetMapping("/group1")
+    @ResponseBody
     @PreAuthorize("hasRole('ROLE_group1')")
     public String group1(Principal principal) {
         return "hello Group1 users, this is the " + principal.getName();
     }
 
     @GetMapping("/group2")
+    @ResponseBody
     @PreAuthorize("hasRole('ROLE_group2')")
     public String group2() {
         return "hello Group2  users";
